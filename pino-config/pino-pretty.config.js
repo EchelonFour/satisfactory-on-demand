53b003e968ceafafb7d57a576b276dfbba50b81a
Frank Fenton <echelon.iiii@gmail.com>
2021-12-12 15:49:44 +1100

    🔊 Spruce up the logs

--- pino-config/pino-pretty.config.js
+++ pino-config/pino-pretty.config.js
@@ -1,16 +1,18 @@
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
 // eslint-disable-next-line import/no-extraneous-dependencies -- this is a dev only file
-const chalk = require('chalk')
+const colorette = require('colorette')
 
 module.exports = {
-  messageFormat(log, messageKey, levelLabel) {
+  messageFormat(log, messageKey, _levelLabel) {
     const logParts = []
-    logParts.push(chalk.magenta`[${log.module || 'global'}]`)
-    if (log.guild) {
-      // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-      logParts.push(chalk.yellow(`{Guild: ${log.guild}}`))
+    logParts.push(colorette.magenta(`[${log.module || 'global'}]`))
+    if (log.envoyPid || log.envoyModule) {
+      logParts.push(colorette.underline(colorette.gray(`${log.envoyPid}|${log.envoyModule}`)))
     }
     logParts.push(log[messageKey])
     return logParts.join(' - ')
   },
-  ignore: 'pid,hostname,module,guild',
+  ignore: 'pid,hostname,module,envoyPid,envoyModule',
+  translateTime: 'SYS:standard',
 }
+/* eslint-enable @typescript-eslint/restrict-template-expressions */
