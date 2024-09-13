8f4d90c4d8c05dbc8a07c473bbb039c18e086807
Frank Fenton <echelon.iiii@gmail.com>
2024-09-13 19:03:47 +1000

    ✨ Update fake query server

--- src/fake-query-server.ts
+++ src/fake-query-server.ts
@@ -1,58 +1,118 @@
 import dgram from 'dgram'
 import { SocketAsPromised, DgramAsPromised } from 'dgram-as-promised'
+import { SmartBuffer } from 'smart-buffer'
 import config from './config.js'
 import globalLogger from './logger.js'
 
 const logger = globalLogger.child({ module: 'fake-query' })
-const MAGIC_RESPONSE = Buffer.from('03', 'hex')
+const PROTOCOL_MAGIC = 0xf6d5 // 0xF6D5
+const PROTOCOL_VERSION = 1
+const TERMINATOR_BYTE = 1
+const ENCODING = 'utf-8' as const
+
+enum SatisfactoryMessageType {
+  PollServerState = 0,
+  ServerStateResponse = 1,
+}
+
+enum SatisfactoryServerState {
+  Offline = 0,
+  Idle = 1,
+  Loading = 2,
+  Playing = 3,
+}
 
 export class FakeQueryServer {
-  protected socket: SocketAsPromised | null = null
+  protected socketIPv4: SocketAsPromised | null = null
 
-  protected readonly beaconPort: Buffer
-
-  protected readonly version: Buffer
+  protected socketIPv6: SocketAsPromised | null = null
 
   constructor(
-    protected port: number = config.get('fakeServerPort'),
-    gamePort: number = config.get('gamePort'),
-    version: number = config.get('fakeQueryVersionResponse'),
-  ) {
-    this.beaconPort = Buffer.alloc(2)
-    this.beaconPort.writeUInt16LE(gamePort)
-    // eslint-disable-next-line @typescript-eslint/no-magic-numbers
-    this.version = Buffer.alloc(4)
-    this.version.writeInt32LE(version)
-  }
+    protected port = config.get('fakeServerPort'),
+    protected fakeVersion = config.get('fakeQueryVersionResponse'),
+    protected fakeName = config.get('fakeServerNameResponse'),
+  ) {}
 
   public async start(): Promise<void> {
-    if (this.socket != null) {
+    if (this.socketIPv4 != null) {
       await this.stop()
     }
-    this.socket = DgramAsPromised.createSocket('udp4', (msg, rinfo) => {
-      void this.messageHandler(msg, rinfo)
+    this.socketIPv4 = DgramAsPromised.createSocket('udp4', (msg, rinfo) => {
+      if (!this.socketIPv4) {
+        return
+      }
+      void this.messageHandler(this.socketIPv4, msg, rinfo)
     })
-    await this.socket.bind(this.port)
+    await this.socketIPv4.bind(this.port)
+    this.socketIPv6 = DgramAsPromised.createSocket('udp6', (msg, rinfo) => {
+      if (!this.socketIPv6) {
+        return
+      }
+      void this.messageHandler(this.socketIPv6, msg, rinfo)
+    })
+    await this.socketIPv6.bind(this.port)
     logger.info('now listening for query connections')
   }
 
-  protected async messageHandler(msg: Buffer, rinfo: dgram.RemoteInfo): Promise<void> {
-    if (!this.socket) {
-      throw new Error('tried to respond on a closed socket')
+  protected async messageHandler(socket: SocketAsPromised, msg: Buffer, rinfo: dgram.RemoteInfo): Promise<void> {
+    const cookie = this.parsePollRequest(msg)
+    if (cookie == null) {
+      return
     }
-    const response = Buffer.concat([msg, MAGIC_RESPONSE, this.version, this.beaconPort]) // add the magic on the end
-    response.writeUInt8(msg.readUInt8(0) + 1, 0) //add 1 to the first byte (maybe should be set to 1 🤷‍♀️)
+    const response = this.buildServerStateResponse(cookie)
     logger.trace({ request: msg, response }, 'sending back query response')
     try {
-      await this.socket.send(response, rinfo.port, rinfo.address)
+      await socket.send(response, rinfo.port, rinfo.address)
     } catch (error) {
       logger.warn({ error }, 'failed to send back response from query')
     }
   }
 
+  protected parsePollRequest(msg: Buffer): bigint | null {
+    const request = SmartBuffer.fromBuffer(msg)
+    if (request.readUInt16LE() !== PROTOCOL_MAGIC) {
+      logger.info('received incorrect magic in a query. Either a rouge probe or the protocol changed')
+      return null
+    }
+    if (request.readUInt8() !== SatisfactoryMessageType.PollServerState) {
+      return null
+    }
+    if (request.readUInt8() !== PROTOCOL_VERSION) {
+      return null
+    }
+    const cookie = msg.readBigUInt64LE()
+    if (request.readUInt8() !== TERMINATOR_BYTE) {
+      return null
+    }
+    if (request.remaining() !== 0) {
+      return null
+    }
+    return cookie
+  }
+
+  protected buildServerStateResponse(cookie: bigint): Buffer {
+    const response = new SmartBuffer()
+    response.writeUInt16LE(PROTOCOL_MAGIC) //ProtocolMagic
+    response.writeUInt8(SatisfactoryMessageType.ServerStateResponse) //MessageType
+    response.writeUInt8(PROTOCOL_VERSION) //ProtocolVersion
+    // Payload
+    response.writeBigInt64LE(cookie) //Cookie
+    response.writeUInt8(SatisfactoryServerState.Loading) //ServerState
+    response.writeUInt32LE(this.fakeVersion) //ServerNetCL
+    // set no flags
+    response.writeBigUInt64LE(BigInt(0)) //ServerFlags
+    //return no states
+    response.writeUInt8(0) // NumSubStates
+    response.writeUInt16LE(Buffer.byteLength(this.fakeName, ENCODING)) //ServerNameLength
+    response.writeString(this.fakeName, ENCODING) //ServerName
+    response.writeUInt8(TERMINATOR_BYTE) //Terminator Byte
+    return response.toBuffer()
+  }
+
   public async stop(): Promise<void> {
-    const { socket } = this
-    this.socket = null
-    await socket?.close()
+    const { socketIPv4, socketIPv6 } = this
+    this.socketIPv4 = null
+    this.socketIPv6 = null
+    await Promise.all([socketIPv4?.close(), socketIPv6?.close()])
   }
 }
